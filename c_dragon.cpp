9956e31ac55c0ab74ca183b2527e37c21f008a78
Anthony Jatobá <anthonyjatoba@gmail.com>
2016-09-15 22:28:15 -0300

    Add dragon of loowater solution

---
+++ c_dragon.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n, m, d, h;
+
+  do {
+
+    cin >> n >> m;
+
+    //indica fim dos casos
+    if (n == 0 && m == 0)
+      break;
+
+    vector<int> diameter;
+    vector<int> height;
+
+    for (int i = 0; i < n; ++i){
+      cin >> d;
+      diameter.push_back(d);
+    }
+
+    for (int i = 0; i < m; ++i){
+      cin >> h;
+      height.push_back(h);
+    }
+
+    //Ordena os vectors
+    sort(diameter.begin(), diameter.begin()+n);
+    sort(height.begin(), height.begin()+m);
+
+    bool win = true;
+    int gold = 0;
+
+    //Percorro todas as cabeças
+    for (int i = 0, int j = 0; i < n && win == true; ++i){
+      //Acabaram os cavaleiros, mas se estou dentro do loop, não acabaram as cabeças!
+      if (j == m){
+        win = false;
+        break;
+      } else if (height[j] >= diameter[i]){   //se o cavaleiro atual pode vencer a cabeça atual
+        gold += height[j];
+        j++;
+      } else {                                //se o cavaleiro não consegue, vou para o próximo (j++).
+        j++;
+        i--;                                  //diminuo i porque o for vai aumentar. Dessa maneira, o valor permanece inalterado.
+      }
+    }
+
+    if (win)
+      cout << gold << endl;
+    else
+      cout << "Loowater is doomed!" << endl;
+
+  } while(n != 0 && m != 0);
+
+}
